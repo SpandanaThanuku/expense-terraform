354737a82ca616753bd6c75b061570ad65a44e69
Spandana <spandanathanuku3@gmail.com>
2025-12-17 19:47:15 +0530

    started creating RDS (Relational Database Store) taken the db subnets as output from VPC module and provided as reference input to RDS opened port 3306 to outside as mysql db is in 3306 port (means set inbound and outbound rules for security group)
    creating app module for creating backend and frontend nodes

--- main.tf
+++ main.tf
@@ -28,9 +28,9 @@ module "rds" {
   project_name  = var.project_name
   kms_key_id    = var.kms_key_id
 
-  subnet_ids     = lookup(lookup(module.vpc, "main", null), "web_subnets_ids", null)
+  subnet_ids     = lookup(lookup(module.vpc, "main", null), "app_subnets_ids", null)
   vpc_id         = lookup(lookup(module.vpc, "main", null), "vpc_id", null)
-  sg_cidr_blocks = lookup(lookup(var.vpc, "main", null), "web_subnets_cidr", null)
+  sg_cidr_blocks = lookup(lookup(var.vpc, "main", null), "app_subnets_cidr", null)
 }
 
 module "app" {
