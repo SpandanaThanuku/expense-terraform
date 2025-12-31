1213d093a875d87d2991f3088a7dd4ebdacdac56
Spandana <spandanathanuku3@gmail.com>
2025-12-31 21:06:30 +0530

    creating userdata file so that the script will run automatically
    added role and policy details

--- main.tf
+++ main.tf
@@ -48,7 +48,7 @@ module "backend" {
   sg_cidr_blocks      = lookup(lookup(var.vpc, "main", null), "app_subnets_cidr", null)
   vpc_id              = lookup(lookup(module.vpc, "main", null), "vpc_id", null)
   vpc_zone_identifier = lookup(lookup(module.vpc, "main", null), "app_subnets_ids", null)
-  parameters          = ["arn:aws:ssm:us-east-1:348220191398:parameter/${var.env}.${var.project_name}.rds"]
+  parameters          = ["arn:aws:ssm:us-east-1:348220191398:parameter/${var.env}.${var.project_name}.rds.*"]
 }
 
 module "frontend" {
