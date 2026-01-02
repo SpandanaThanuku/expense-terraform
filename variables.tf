e64da4f49d07a94b1185a49f2cb1d4490e2d7c0c
Spandana <spandanathanuku3@gmail.com>
2026-01-02 22:38:28 +0530

    creating userdata file so that the script will run automatically
    added role and policy details

--- variables.tf
+++ variables.tf
@@ -6,13 +6,13 @@ variable "bastion_cidrs" {}
 variable "vpc" {}
 variable "rds" {}
 
-variable "backend_app_port" {}
+/*variable "backend_app_port" {}
 variable "backend_instance_capacity" {}
 variable "backend_instance_type" {}
 
 variable "frontend_app_port" {}
 variable "frontend_instance_capacity" {}
-variable "frontend_instance_type" {}
+variable "frontend_instance_type" {} */
 
 variable "acm_arn" {}
 
